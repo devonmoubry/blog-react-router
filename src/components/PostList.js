b1ff439c4861c95afa72490bf40c0e602c9a203f
Devon Moubry <devon@moubry.com>
2017-09-23 23:35:22 -0500

    render list of posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class List extends Component {
+  render() {
+    let blogs = this.props.blogs;
+    let List = blogs.map(blog => {
+      return (
+        <li key={blog._id}>
+          <Link to={`/show/${blog._id}`}>{blog.blogTitle}</Link>
+        </li>
+      );
+    });
+    return (
+      <div>
+        {List}
+      </div>
+    )
+  }
+}
 
 export default class PostList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogs: []
+    };
+  }
+
+  componentDidMount() {
+    let url = "https://tiny-lasagna-server.herokuapp.com/collections/blogger/";
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      this.setState({ blogs: data });
+    })
+    .catch(error => {
+      console.log("Error with Fetching : ", error);
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>PostList</h1>
+        <div>
+          <List blogs={this.state.blogs} />
+        </div>
       </div>
     );
   }
+
 }
